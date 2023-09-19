b1a77d4512968fd2530105fd3f55dee7b380e3e3
Mateus <shakii876954@gmail.com>
2023-09-18 22:08:33 -0300

    probb the last one

--- src/layout/components/LeftBar/TabContainer.tsx
+++ src/layout/components/LeftBar/TabContainer.tsx
@@ -3,6 +3,9 @@ import Tab from "./Tab";
 import type { geralProps } from "~/libs/interfaces/geralProps";
 import UserProfile from "./UserProfile";
 import Tabs from "~/utils/constants/Tabs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import type { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { signOut } from "next-auth/react";
 
 interface props extends geralProps {
   show: boolean;
@@ -14,6 +17,21 @@ function TabContainer({ ...props }: props) {
       {Tabs.map((T, i) => (
         <Tab {...props} key={i} {...T} />
       ))}
+      <button
+        className="flex scale-[0.975] items-center gap-x-2 rounded-full opacity-75 transition-all
+        hover:scale-100 hover:bg-white/5 hover:opacity-100"
+        onClick={() => {
+          signOut().catch((err) => console.error(err));
+        }}
+      >
+        <div className="flex h-12 w-12 flex-shrink-0 items-center justify-center overflow-hidden rounded-full bg-purple">
+          <FontAwesomeIcon
+            icon={"fa-right-from-bracket" as IconProp}
+            className="h-9 w-9 flex-shrink-0 rounded-full text-lightGolden"
+          />
+        </div>
+        {props.show && <p className="font-semibold text-purple">Sign Out</p>}
+      </button>
       <UserProfile {...props} />
     </div>
   );
--- src/libs/setups/Fonts.ts
+++ src/libs/setups/Fonts.ts
@@ -1,16 +1,28 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHouse, faMedal, faClock, faMagnifyingGlass, faPlus, faCircleXmark, faTrash, faSquareXmark, faSquareCheck } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHouse,
+  faMedal,
+  faClock,
+  faMagnifyingGlass,
+  faPlus,
+  faCircleXmark,
+  faTrash,
+  faSquareXmark,
+  faSquareCheck,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
-export default function icons () {
-    library.add(
-        faHouse,
-        faMedal,
-        faClock,
-        faMagnifyingGlass,
-        faPlus,
-        faCircleXmark,
-        faTrash,
-        faSquareXmark,
-        faSquareCheck
-    )
+export default function icons() {
+  library.add(
+    faHouse,
+    faMedal,
+    faClock,
+    faMagnifyingGlass,
+    faPlus,
+    faCircleXmark,
+    faTrash,
+    faSquareXmark,
+    faSquareCheck,
+    faRightFromBracket
+  );
 }
