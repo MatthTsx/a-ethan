ae2038301d95650db9946f35d286b893cbaab6a4
Mateus Almeida Oliveira <shakii876954@gmail.com>
2023-09-27 07:44:57 -0300

    Update SignProvider.tsx

--- src/layout/providers/SignProvider.tsx
+++ src/layout/providers/SignProvider.tsx
@@ -12,13 +12,16 @@ function SignProvider({ ...props }: props) {
 
   if (!session.data?.user) {
     return (
+      <div className="w-screen h-screen flex items-center justify-center">
       <button
         onClick={() => {
           signIn("google").catch((err) => console.error(err));
         }}
+        className={"bg-lightGolden flex items-center justify-center shadow-lg px-8 p-4 rounded-full hover:scale-105 transition-all"}
       >
         Sign in
       </button>
+      </div>
     );
   }
 
